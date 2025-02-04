11d07f9f1b04fd6fbe37bb8ad74091cfd3daecbf
GroupB-499 <alsheikh.noura0@gmail.com>
2025-02-04 23:58:24 +0500

    update

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./ridepsu-8b9fc-firebase-adminsdk-fbsvc-cba71b735d.json");
+const serviceAccount = require("./ridepsu-8b9fc-firebase-adminsdk-fbsvc-06a5286b53.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
